9bb470670a4e8d3eff5b39af1ac7054a443c3122
Tyler Baumler <54323980+tyler-willowtree@users.noreply.github.com>
2023-06-14 14:35:47 -0400

    added update to each row

--- server/assets/main.js
+++ server/assets/main.js
@@ -1,5 +1,7 @@
 /* This file is in the head of the html file */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+const formTypeOptions = ['create', 'update'];
+
 const queries = {
   toggle: (id) => `mutation ToggleFlag {
     toggleFlag(id: ${id}) {
@@ -19,6 +21,15 @@ const queries = {
       updatedAt
     }
   }`,
+  update: (id, data) => `mutation updateFlag {
+    updateFlag(id: ${id}, ${data.join(', ')}) {
+      id
+      name
+      description
+      enabled
+      updatedAt
+    }
+  }`,
   delete: (id) => `mutation DeleteFlag {
     deleteFlag(id: ${id}) {
       id
@@ -29,7 +40,7 @@ const queries = {
     }
   }`,
 };
-
+/// mutation updateFlag {\n updateFlag(id: 6, data: name: "jjdkd-khbjhb", description: "lksdjhfkwjn") {\n id\n name\n description\n enabled\n updatedAt\n }\n }'
 const removeRegex = /[^A-Za-z0-9-]/gi;
 
 const changeToParamCase = (str) => {
@@ -55,18 +66,16 @@ const whichToggle = (flag) => {
   return 'fa-toggle-on';
 };
 
-const createPost = (query) => {
-  return {
-    url: '/graphql',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ query }),
-  };
+const getFormElements = () => {
+  const wrapper = document.getElementById('form');
+  const form = wrapper.querySelector('form');
+  const nameField = form.querySelector('#name');
+  const descriptionField = form.querySelector('#description');
+
+  return { descriptionField, form, nameField, wrapper };
 };
 
-const createRow = (flag) => {
+const createRowHtml = (flag) => {
   return `
     <tr data-row='${flag.id}'>
       <td>${flag.name}</td>
@@ -79,10 +88,16 @@ const createRow = (flag) => {
       <td class='text-right'>${formatDate(flag.updatedAt)}</td>
       <td>
         <div style='display: flex; column-gap: 16px;'>
-          <button class='icon' onclick='deleteFlag(${flag.id})'>
+          <button class='icon delete' onclick='callDeleteFlag(${flag.id})'>
             <i class='fa-solid fa-xmark fa-xl'></i>
           </button>
-          <button class='icon toggle' onclick='toggleFlag(${flag.id})'>
+          <button
+            class='icon edit'
+            onclick='toggleFormOpen("update", ${flag.id})'
+          >
+            <i class='fa-solid fa-pen'></i>
+          </button>
+          <button class='icon toggle' onclick='callToggleFlag(${flag.id})'>
             <i class='fa-solid ${whichToggle(flag)} fa-lg'></i>
           </button>
         </div>
@@ -91,11 +106,34 @@ const createRow = (flag) => {
   `;
 };
 
-const toggleFlag = (id) => {
+const getApiConfig = (query) => {
+  return {
+    url: '/graphql',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  };
+};
+
+/** -- Flags from server -- */
+let flagToEdit = null;
+const str = window.flags.replaceAll('&#34;', '"');
+const flags = JSON.parse(str);
+const allFlags = [];
+flags
+  .sort((a) => (a.enabled ? -1 : 1))
+  .forEach((flag) => {
+    allFlags.push(createRowHtml(flag));
+  });
+
+/** -- API Calls -- */
+const callToggleFlag = (id) => {
   const table = document.querySelector('tbody');
   const row = document.querySelector(`[data-row="${id}"]`);
   const index = row.rowIndex;
-  const { url, body, headers, method } = createPost(queries.toggle(id));
+  const { url, body, headers, method } = getApiConfig(queries.toggle(id));
 
   fetch(url, {
     method,
@@ -104,14 +142,16 @@ const toggleFlag = (id) => {
   })
     .then((res) => res.json())
     .then((result) => {
-      table.insertRow(index - 1).innerHTML = createRow(result.data.toggleFlag);
+      table.insertRow(index - 1).innerHTML = createRowHtml(
+        result.data.toggleFlag
+      );
       table.deleteRow(index);
     })
     .catch((err) => console.log(err));
 };
 
-const deleteFlag = (id) => {
-  const { url, body, headers, method } = createPost(queries.delete(id));
+const callDeleteFlag = (id) => {
+  const { url, body, headers, method } = getApiConfig(queries.delete(id));
   fetch(url, {
     method,
     headers,
@@ -124,35 +164,76 @@ const deleteFlag = (id) => {
     .catch((err) => console.log(err));
 };
 
-const addNewFlag = () => {
-  document.getElementById('form').classList.remove('notEnabled');
+/** -- Form -- */
+const toggleFormOpen = (which, id = null) => {
+  const { descriptionField, form, nameField, wrapper } = getFormElements();
+
+  if (formTypeOptions.includes(which)) {
+    if (wrapper.classList.contains(which)) {
+      wrapper.classList.remove(which);
+      wrapper.classList.add('hidden');
+      form.reset();
+    } else {
+      if (which === 'update' && id) {
+        flagToEdit = flags.find((flag) => flag.id === id);
+        nameField.value = flagToEdit.name;
+        descriptionField.value = flagToEdit.description;
+      }
+      wrapper.classList.add(which);
+      wrapper.classList.remove('hidden');
+    }
+  }
+};
+
+const getWhichFormIsOpen = () => {
+  const { wrapper } = getFormElements();
+  const formClassList = wrapper.classList;
+  return formTypeOptions.find((option) => formClassList.contains(option));
+};
+
+const handleCancelForm = () => {
+  const opt = getWhichFormIsOpen();
+  if (opt) {
+    toggleFormOpen(opt);
+  }
 };
 
 const submitForm = () => {
-  const theForm = document.getElementById('form').querySelector('form');
-  const formData = new FormData(theForm);
+  const { form } = getFormElements();
+  const whichCall = getWhichFormIsOpen();
+  const formData = new FormData(form);
   const data = [];
   for (const [key, value] of formData.entries()) {
     data.push(`${key}: ${typeof value === 'string' ? `"${value}"` : value}`);
   }
 
-  const { url, body, headers, method } = createPost(queries.create(data));
-  fetch(url, {
-    method,
-    headers,
-    body,
+  let callData;
+  if (whichCall === 'create') {
+    callData = getApiConfig(queries.create(data));
+  } else {
+    callData = getApiConfig(queries.update(flagToEdit.id, data));
+  }
+  fetch(callData.url, {
+    method: callData.method,
+    headers: callData.headers,
+    body: callData.body,
   })
     .then((res) => res.json())
     .then((result) => {
-      theForm.reset();
-      document.querySelector('tbody').insertRow(0).innerHTML = createRow(
-        result.data.createFlag
-      );
-      document.getElementById('form').classList.add('notEnabled');
+      if (whichCall === 'create') {
+        document.querySelector('tbody').insertRow(0).innerHTML = createRowHtml(
+          result.data.createFlag
+        );
+      } else {
+        const row = document.querySelector(`[data-row="${flagToEdit.id}"]`);
+        row.innerHTML = createRowHtml(result.data.updateFlag);
+      }
+      handleCancelForm();
     })
     .catch((err) => console.log(err));
 };
 
+/** -- Search -- */
 const handleSearch = () => {
   const input = document.getElementById('search');
   const filter = input.value.toUpperCase();
@@ -174,7 +255,7 @@ const handleSearch = () => {
   });
 };
 
-const clearSearch = () => {
+const handleClearSearch = () => {
   document.getElementById('search').value = '';
   handleSearch();
 };
