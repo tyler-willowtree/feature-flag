2f665681301382c024649aa20e9b279082e585bd
Tyler Baumler <54323980+tyler-willowtree@users.noreply.github.com>
2023-06-16 15:19:19 -0400

    added single db option

--- server/app.controller.ts
+++ server/app.controller.ts
@@ -1,13 +1,7 @@
 import { Controller, Get, Render } from '@nestjs/common';
-import { FeatureFlagService } from 'server/featureFlag.service';
-import { FeatureFlagSingleDbService } from 'server/featureFlagSingleDb.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly service: FeatureFlagService,
-    private readonly singleService: FeatureFlagSingleDbService
-  ) {}
 
   @Get()
   @Render('index')
