cb2e957f71a5cec537d2388e03721838da243571
Tyler Baumler <54323980+tyler-willowtree@users.noreply.github.com>
2023-06-28 13:16:31 -0400

    updated server side page to break out into multiple pages

---
+++ server/assets/components/flagged-feature.js
@@ -0,0 +1,45 @@
+customElements.define(
+  'flagged-feature',
+  class extends HTMLElement {
+    connectedCallback() {
+      const flag = this.getAttribute('flag-key');
+      if (flag) {
+        return new Promise(() => {
+          fetch('/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              query: `{
+              getFlagByName(name: "${flag}") {
+                id
+                name
+                enabled
+              }
+            }`,
+            }),
+          })
+            .then((res) => res.json())
+            .then((result) => {
+              if (
+                result.data.getFlagByName &&
+                !result.data.getFlagByName.enabled
+              ) {
+                this.innerHTML =
+                  this.querySelector('[slot="feature"]').innerHTML;
+              } else {
+                const elseElement = this.querySelector('[slot="elseElement"]');
+                if (elseElement) {
+                  this.innerHTML = elseElement.innerHTML;
+                } else {
+                  this.remove();
+                }
+              }
+            })
+            .catch((err) => {
+              this.innerHTML = `<p>Something went wrong (${err})</p>`;
+            });
+        });
+      }
+    }
+  }
+);
